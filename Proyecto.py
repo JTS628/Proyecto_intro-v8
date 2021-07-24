d6d12bff0a7d7276c3f55578ed69a12af7cfe1a4
JTS628 <jj628@hotmail.com>
2021-07-24 00:31:58 -0600

    actualizacion
    actualizacion funcion modificar y funcion busqueda

--- Proyecto.py
+++ Proyecto.py
@@ -36,7 +36,7 @@ def Menu ():
         print ("1. Ingresar producto")
         print ("2. Consulta producto")
         print ("3. Elimiar producto")
-        print ("4. Mostrar inventario")
+        print ("4. Modificar inventario")
         print ("0. Salir")
         seleccion_Opciones = int(input("Ingrese una opcion: "))
 
@@ -46,6 +46,9 @@ def Menu ():
             consultar_inventario()
         elif seleccion_Opciones == 3:
             Elimiar_producto()
+
+        elif seleccion_Opciones == 4:
+            Modificar_inventario ()
 
         elif seleccion_Opciones == 5:
             print ("Salir")
@@ -64,34 +67,50 @@ def ingreso_Producto():
     #lista_Precio_Producto = []
 
     while condicion == "si":
-        nombre = input ("ingrese el nombre")
-        codigo = input ("ingrese el codigo")
-        cantidad = input ("ingrese la cantidad")
-        precio = input ("ingrese la cantidad")
+        nombre = input ("ingrese el nombre: ")
+        
+        codigo = input ("ingrese el codigo: ")
+        
+        cantidad = input ("ingrese la cantidad: ")
+      
+        precio = input ("ingrese la cantidad: ")
+        
         Inventario.append(nombre)
         Inventario.append(codigo)
         Inventario.append(cantidad)
         Inventario.append(precio)
-    
+              
+
+        with open ("registroprueba.txt","a") as archivo:
+            archivo.write(str(Inventario))
+            archivo.write("\n")
+
+        Menu()
+           
+        #condicion = input(print ("Desea agregar un producto? Si/No"))
         #lista_Nombre_Producto.append (input( "Ingrese el nombre del producto: "))
         #lista_Codigo_Producto.append (int(input("Ingrese el codigo del producto ")))
         #lista_Cantidad_Producto.append (int(input("Ingrese la cantidad de unidades ")))
         #lista_Precio_Producto.append(int (input("Ingrese el precio del producto ")))
-        condicion = input(print ("Desea agregar un producto? Si/No"))
-    else:
-        Menu ()
-    with open ("registroprueba.txt","a") as archivo:
-        archivo.write(str(Inventario))
+    Menu ()  
+    #else:
+        
+    
 
 
 
 def consultar_inventario (): 
+    
     abrir = open("registroprueba.txt","r")
     consulta = abrir.read()
-   # for I in consulta:
-    #    print (I)
     print (consulta)
+    
+    #for I in consulta.readlines():
+    #    print (I)
+        
     abrir.close()
+
+
     
     
     #for I in lista_Nombre_Producto:
@@ -102,4 +121,34 @@ def consultar_inventario ():
 def Elimiar_producto ():
     print ("Mae meta su codigo aqui")
 
-Menu ()
+
+def Modificar_inventario ():
+    read = open("registroprueba.txt","r")
+    reemplazo = open ("temporal","w")
+
+    busqueda = input ("que articulo desea modificar: ")
+
+    s = " "
+
+    while (s):
+        s=read.readline()
+        L=s.split(",")
+        
+        if len (s) > 0:
+            if L [0] == busqueda:
+                nombre = input ("ingrese el nombre ")
+                codigo = input ("ingrese el codigo ")
+                cantidad = input ("ingrese la cantidad ")
+                precio = input ("ingrese la cantidad ")
+                reemplazo.write(nombre +","+codigo+","+cantidad+","+precio+"\n")
+            else:
+                reemplazo.write(s)
+    read.close()
+    reemplazo.close()
+    os.remove("registroprueba.txt")
+    os.rename("temporal","registroprueba.txt")
+
+
+#consultar_inventario ()
+Modificar_inventario ()
+#Menu ()
