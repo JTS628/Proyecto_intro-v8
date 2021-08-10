14f488fa4ed7faf3bef40061d8a54e31ea438212
JTS628 <jj628@hotmail.com>
2021-08-09 21:59:08 -0600

    Actualizacion
    actualizacion a plataforma visual

--- prueba if.py
+++ prueba if.py
@@ -1,10 +1,46 @@
+import os
+import sys
+from tkinter import *
+from tkinter import font 
+from PIL import Image
+from tkinter import ttk 
 
-with open ("registroprueba.txt","r") as read:
-    leer = read.read()
-    print (leer[1])
-    read.close()
+root = Tk()
+root.title("ventana Principal")
+root.geometry("800x600")
+root.config(background="#202020")
+ 
+ 
+with open("registroprueba.txt","r") as read:
 
-#ld_lista = ttk.Combobox(root,values=lista)
-#ld_lista.pack()
+    s = " "
+    s=read.readline()
+    L=s.split(",")
+
+    for Item in L:
+        
+        print (Item[0])
+
+
+
+    """
+    while (s):
+        s=read.readline()
+        global pr
+        
+        pr = L[0]
+        #fr = list(pr)
+        print (pr)
+       
+
+    ld_lista = ttk.Combobox(root,values=pr)
+    ld_lista.pack()
+    """
+        
+#ld_lista2 = StringVar()
+#ld_lista2.set(pr[0])
+#dropotion = OptionMenu(root,value=pr,*pr)
+#dropotion.pack(pady=50)
+       
 
 #root.mainloop()
