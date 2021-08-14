9b26c0314ff1002dcdbb0064b0dfc1362d5f8bfe
esteban2331 <info.clases2331@gmail.com>
2021-08-13 22:37:23 -0600

    pruebas
    se hicieron pruebas a ventana eliminar
    (aun no esta terminado)

--- Plataformagrafica testv1.py
+++ Plataformagrafica testv1.py
@@ -2,7 +2,7 @@ import os
 import sys
 from tkinter import *
 from tkinter import font 
-from PIL import Image
+#from PIL import Image
 from tkinter import ttk 
 
 root = Tk()
@@ -198,19 +198,47 @@ def ventana_consulta():
       
 ventana_elim = ""
 def ventana_eliminar():
-    global ventana_elim
-    ventana_elim = Toplevel(root)
+    global ventana_mod
+    ventana_mod = Toplevel(root)
     root.withdraw()
-    ventana_elim.geometry("600x600")
-    ventana_elim.title("Eliminacion de Productos")
-    ventana_elim.config(background="#202020")
+    ventana_mod.geometry("600x600")
+    ventana_mod.title("Modificacion de productos")
+    ventana_mod.config(background="#202020") 
 
-    lblfotodel = Label(ventana_elim,image=fotodel)
-    lblfotodel.pack()
+    lblfotomod = Label(ventana_mod,image=fotomod)
+    lblfotomod.pack()
+
+    bt_cerrar_ingreso=Button(ventana_mod,text="Regresar",command=regreso_mod,font=("Times new Roman","12"),bg="#E0E0E0")
+    bt_cerrar_ingreso.place(x="350",y="500")
+
+    global ent_modcual
+    global ent_modnom
+    global ent_modcod
+    global ent_modcant
+    global ent_modpre
+
+    lbl_mod = Label(ventana_mod,text="Eliminacion de productos",font=("Arial","22"),fg="white",bg="#202020")
+    lbl_modcual = Label(ventana_mod,text="Ingrese el producto a eliminar",font=("Arial","14"),fg="#FF6666",bg="#202020")
+    ent_modcual = Entry(ventana_mod,font=("Albertus Extra Bold","14"),width="18",borderwidth="5")  
+       
+  
+    btn_aceptar = Button(ventana_mod, command=os.remove,text="Eliminar",font=("comic","12"),bg="red")
+
+    #global ld_lista
+    #ld_lista = ttk.Combobox(ventana_mod,values=Listita)
+    #ld_lista.current(0)
+    #ld_lista.bind("<ComboboxSelected>",comboclick)
+    #ld_lista.pack()
+        
     
+    
+    lbl_mod.place(x="100",y="50")
+    lbl_modcual.place(x="30",y="150")
+    ent_modcual.place(x="350",y="150")
 
-    bt_cerrar_ingreso=Button(ventana_elim,text="Regresar",command=regreso_elim,font=("Arial","14"),foreground="#193300")
-    bt_cerrar_ingreso.place(x=250, y=500 )
+    btn_aceptar.place(x="250",y="500")
+
+
 
 def lista():
     with open("registroprueba.txt","r") as read:
@@ -296,7 +324,7 @@ def ventana_modificar():
 lbl_opciones=Label(root,text="Menu de opciones",font=("Helvetica","19"),foreground="#CCFFE5",background="#052851",height="1",width="55")
 bt_ingreso=Button(root,text="Ingresar",command=ventana_ingreso,font=("Helvetica","17"),foreground="#CCFFE5",background="#808080",height="1",width="10")
 bt_consulta=Button(root,text="Consultar",command=ventana_consulta,font=("Helvetica","17"),foreground="#CCFFE5",background="#808080",height="1",width="10")
-bt_eliminar=Button(root,text="Elimiar",command=ventana_eliminar,font=("Helvetica","17"),foreground="#CCFFE5",background="#808080",height="1",width="10")
+bt_eliminar=Button(root,text="Eliminar",command=ventana_eliminar,font=("Helvetica","17"),foreground="#CCFFE5",background="#808080",height="1",width="10")
 bt_modificar=Button(root,text="Modificar",command=ventana_modificar,font=("Helvetica","17"),foreground="#CCFFE5",background="#808080",height="1",width="10")
 lbl_opciones.place(y="50")
 bt_ingreso.place(x="100", y="450")
